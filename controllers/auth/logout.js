35d104e66cfde1272e3cf7f9b9d60a6e3999c0bb
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-10-07 17:40:10 +0300

    Створює logout.

---
+++ controllers/auth/logout.js
@@ -0,0 +1,11 @@
+const { User } = require("../../models/user");
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.json({
+    messege: "Logout success",
+  });
+};
+
+module.exports = logout;
