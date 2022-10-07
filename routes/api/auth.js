daba20830a1bbf044d44e8f35f3c8d24c322d170
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-10-07 17:34:18 +0300

    Cтворює запит актуального користувача.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/auth");
 
 const { ctrlWrapper } = require("../../helpers");
 
-const { validateBody } = require("../../middelwares");
+const { validateBody, authenticate } = require("../../middelwares");
 
 const { schemas } = require("../../models/user");
 
@@ -21,5 +21,7 @@ router.post(
   validateBody(schemas.loginSchema),
   ctrlWrapper(ctrl.login)
 );
+
+router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 
 module.exports = router;
